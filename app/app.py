a513775fe4d12a075a46097f030cd3b7f6010005
yauheni.paliakou <yauheni.paliakou@leverx.com>
2024-10-17 13:11:14 +0300

    update check

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 import socket
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route("/")
 def hello():
     hostname = socket.gethostname()
     return render_template("index.html", hostname=hostname)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
