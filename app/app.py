154a339c8e9e5f47ee6b1003acdf620199e20df4
yauheni.paliakou <yauheni.paliakou@leverx.com>
2024-10-17 14:33:12 +0300

    update link

--- app/app.py
+++ app/app.py
@@ -15,13 +15,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    """
-    Renders the home page with "Hello World" and the host name.
-    """
     hostname = socket.gethostname()
     return render_template("index.html", hostname=hostname)
 
 
 if __name__ == "__main__":
-    # Runs the Flask application on port 5000 and makes it accessible from any host.
     app.run(host="0.0.0.0", port=5000)
