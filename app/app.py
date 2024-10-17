71ad86729abcc226703223efd1bfb8fc79d5d3d9
yauheni.paliakou <yauheni.paliakou@leverx.com>
2024-10-17 12:25:38 +0300

    run action

--- app/app.py
+++ app/app.py
@@ -8,6 +8,6 @@ def hello():
     hostname = socket.gethostname()
     return render_template('index.html', hostname=hostname)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
